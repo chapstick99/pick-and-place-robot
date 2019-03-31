0c1feb7588246830b4d2048c651972987adf3974
chapstick99 <49132594+chapstick99@users.noreply.github.com>
2019-03-31 16:46:57 -0700

    Add files via upload

--- motor_sensor_setup.h
+++ motor_sensor_setup.h
@@ -9,32 +9,10 @@ void claw(int power) {
 	motorSet(4, power);
 }
 void loader() {
-	if (digitalRead(2)==0)
-	{
-		return false;
-	}
-	if (digitalRead(2) == 0)
-	{
-		return true;
-	}
-}
+	return digitalRead(2);
 void down() {
-	if (digitalRead(3) == 0)
-	{
-		return false;
-	}
-	if (digitalRead(2) == 0)
-	{
-		return true;
-	}
+	return digitalRead(3);
 }
 void up() {
-	if (digitalRead(1) == 0)
-	{
-		return false;
-	}
-	if (digitalRead(2) == 0)
-	{
-		return true;
-	}
+	return digitalRead(1);
 }
